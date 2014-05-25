2ee3d8eed9776699e2b23b82edab75767dd93d61
Anthony Sciamanna <asciamanna@gmail.com>
2014-05-25 17:46:13 -0400

    adding angularjs app to track music listening.

---
+++ js/listeningto-app/controllers/mainController.js
@@ -0,0 +1,50 @@
+app.controller('mainController', function($scope, $http) {
+    $scope.topArtists = [];
+    $scope.recentTracks = [];
+    $scope.showRecentTracks = true;
+    $scope.showTopArtists = false;
+    $scope.hideRecentTracks = false;
+    $scope.hideTopArtists = true;
+    $scope.recentTracksActive="active";
+    $scope.topArtistsActive="";
+
+    $scope.getRecentTracks = function() {
+        $http.jsonp('http://listeningto.apphb.com/api/recenttracks?callback=JSON_CALLBACK')
+            .success(function(data) {
+                $scope.recentTracks.length = 0;
+                angular.forEach(data, function(track, index) {
+                    track.LastPlayedDate = new Date(track.LastPlayed).toLocaleString();
+                    $scope.recentTracks.push(track);
+                });
+                $scope.showRecentTrakcs = true;
+                $scope.showTopArtists = false;
+                $scope.hideRecentTracks = false;
+                $scope.hideTopArtists = true;
+                $scope.recentTracksActive = "active";
+                $scope.topArtistsActive = "";
+            })
+            .error(function(error) {
+            });
+    };
+
+    $scope.getTopArtists = function() {
+        $http.jsonp('http://listeningto.apphb.com/api/topArtists?callback=JSON_CALLBACK')
+            .success(function(data) {
+                $scope.topArtists.length = 0;
+                angular.forEach(data, function(artist, index) {
+                    $scope.topArtists.push(artist);
+                });
+                $scope.showRecentTrakcs = false;
+                $scope.showTopArtists = true;
+                $scope.hideRecentTracks = true;
+                $scope.hideTopArtists = false;
+                $scope.recentTracksActive = "";
+                $scope.topArtistsActive = "active";
+            })
+            .error(function(error) {
+            });
+    };
+
+    $scope.init = $scope.getRecentTracks();
+
+});
