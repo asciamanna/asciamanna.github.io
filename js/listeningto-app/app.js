00e03c9f11806b9eebd7d1d8672493e92742fa4d
Anthony Sciamanna <asciamanna@gmail.com>
2014-06-20 23:55:31 +0000

    refactoring angularjs controllers.

--- js/listeningto-app/app.js
+++ js/listeningto-app/app.js
@@ -1,1 +1,58 @@
 var app = angular.module('ListeningToApp', ['ngAnimate']);
+
+function mainController($scope, $http) {
+    $scope.topArtists = [];
+    $scope.recentTracks = [];
+    $scope.showRecentTracks = true;
+    $scope.showTopArtists = false;
+    $scope.recentTracksActive = true;
+    $scope.topArtistsActive = false;
+    $scope.connectError = false;
+    $scope.hideSpinner = true;
+
+    $scope.getRecentTracks = function() {
+        $scope.hideSpinner = false;
+        $http.jsonp('http://listeningto.apphb.com/api/recenttracks?callback=JSON_CALLBACK')
+            .success(function(data) {
+                $scope.recentTracks.length = 0;
+                angular.forEach(data, function(track, index) {
+                    track.IsNowPlaying = track.LastPlayed === 'Now Playing';
+                    if(!track.AlbumArtLocation) {
+                        track.AlbumArtLocation = '/images/No_Cover.gif';
+                    }
+                    $scope.recentTracks.push(track);
+                });
+                $scope.showRecentTracks = true;
+                $scope.showTopArtists = false;
+                $scope.recentTracksActive = true;
+                $scope.topArtistsActive = false;
+                $scope.connectError = false;
+                $scope.hideSpinner = true;
+             })
+             .error(function(error) {
+                $scope.connectError = true;
+                $scope.hideSpinner = true;
+             });
+    };   
+
+    $scope.getTopArtists = function() {
+       $scope.hideSpinner = false;
+       $http.jsonp('http://listeningto.apphb.com/api/topArtists?callback=JSON_CALLBACK')
+           .success(function(data) {
+              $scope.topArtists.length = 0;
+              angular.forEach(data, function(artist, index) {
+                 $scope.topArtists.push(artist);
+              });
+              $scope.showRecentTracks = false;
+              $scope.showTopArtists = true;
+              $scope.recentTracksActive = false;
+              $scope.topArtistsActive = true;
+              $scope.connectError = false;
+              $scope.hideSpinner = true;
+            })
+            .error(function(error) {
+               $scope.connectError = true;
+                $scope.hideSpinner = true;
+            });
+    };
+}
