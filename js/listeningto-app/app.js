7b757298a609c3e85861cfcc7fefb72335a56def
Anthony Sciamanna <asciamanna@gmail.com>
2023-03-31 08:59:27 -0400

    adds Top Albums front-end code to the website

--- js/listeningto-app/app.js
+++ js/listeningto-app/app.js
@@ -3,8 +3,11 @@ var app = angular.module('ListeningToApp', ['ngAnimate']);
 function MainController($scope, $http) {
     $scope.topArtists = [];
     $scope.recentTracks = [];
+    $scope.topAlbums = [];
     $scope.displayData = {
        showRecentTracks: true,
+       showTopArtists: false,
+       showTopAlbums: false,
        connectError: false,
        hideSpinner: true
     };
@@ -22,6 +25,8 @@ function MainController($scope, $http) {
                     recentTracks.push(track);
                 });
                 displayData.showRecentTracks = true;
+                displayData.showTopArtists = false;
+                displayData.showTopAlbums = false;
                 displayData.connectError = false;
                 displayData.hideSpinner = true;
              })
@@ -40,6 +45,8 @@ function MainController($scope, $http) {
                  topArtists.push(artist);
               });
               displayData.showRecentTracks = false;
+              displayData.showTopArtists = true;
+              displayData.showTopAlbums = false;
               displayData.connectError = false;
               displayData.hideSpinner = true;
             })
@@ -48,4 +55,24 @@ function MainController($scope, $http) {
                displayData.hideSpinner = true;
             });
     };
+
+    $scope.getTopAlbums = function(topAlbums, displayData) {
+      displayData.hideSpinner = false;
+      $http.get('https://music-stats.herokuapp.com/musicstats/topalbums')
+          .success(function(data) {
+             topAlbums.length = 0;
+             angular.forEach(data.items, function(album, index) {
+                topAlbums.push(album);
+             });
+             displayData.showRecentTracks = false;
+             displayData.showTopArtists = false;
+             displayData.showTopAlbums = true;
+             displayData.connectError = false;
+             displayData.hideSpinner = true;
+           })
+           .error(function(error) {
+              displayData.connectError = true;
+              displayData.hideSpinner = true;
+           });
+   };
 }
